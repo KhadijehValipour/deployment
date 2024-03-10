3980c0fb30d3157b3c4600b1b606fa69287c2439
KhadijehValipour <136600506+KhadijehValipour@users.noreply.github.com>
2024-03-10 06:52:30 -0700

    Delete API directory

--- API/other_languages/number.cs
+++
@@ -1,5 +0,0 @@
-var client = new HttpClient();
-var request = new HttpRequestMessage(HttpMethod.Get, "http://numbersapi.com/42");
-var response = await client.SendAsync(request);
-response.EnsureSuccessStatusCode();
-Console.WriteLine(await response.Content.ReadAsStringAsync());
